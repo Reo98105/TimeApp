7e5aab99644ab35f4f0e7f7a75e23ff01b30a6ec
Reo98105 <gundamzone00@gmail.com>
2020-08-05 01:57:28 +0800

    Working on switch case...

--- TimeApp/MainWindow.xaml.cs
+++ TimeApp/MainWindow.xaml.cs
@@ -1,11 +1,11 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
-using System.Windows;
-using System.Timers;
-using System.Windows.Threading;
 using System.Media;
 using System.Reflection;
-using System.Diagnostics;
+using System.Timers;
+using System.Windows;
+using System.Windows.Threading;
 
 namespace TimeApp
 {
@@ -38,21 +38,21 @@ namespace TimeApp
             timer.Start();
         }
 
-        //event to calculate remaining time for nxt min
+        //event to calculate remaining time for next min (to trigger audio executor i suppose?)
         void timer_elapsed(object source, ElapsedEventArgs e)
         {
             Trace.WriteLine(DateTime.Now.ToString("hh:mm:ss tt"));
             Audio_Executor();
             timer.Interval = Get_Interval();
             timer.Start();
-        } 
+        }
 
         //to get starting interval as soon as the app startup
         private double GetInitialInterval()
         {
             DateTime now = DateTime.Now;                                                 //current local time of the comp
             double timeToNextMin = ((60 - now.Second) * 1000 - now.Millisecond) + 15;    // ((60 sec - current time in sec) * 1000 - current time in ms) + 15
-            nxtInterval = now.Ticks + ((long)timeToNextMin * ticks_in_millisecond);      // nxtinterval = currrent time + abv equation * ticks in ms
+            nxtInterval = now.Ticks + ((long)timeToNextMin * ticks_in_millisecond);      // nxtinterval = currrent time + above equation * ticks in ms
 
             return timeToNextMin;
         }
@@ -71,7 +71,7 @@ namespace TimeApp
             return (double)(ticks / ticks_in_millisecond);
         }
 
-        //Event that happened for clocks' hands ticking and texts that shows digital clock
+        //Event to update clocks' hands ticking and texts that shows digital clock
         void DispatcherTimer_Tick(object sender, EventArgs e)
         {
             double secs = DateTime.Now.Second;
@@ -88,42 +88,37 @@ namespace TimeApp
         {
             audioPlayer = new SoundPlayer();
 
-            /*TimeSpan currentTime = DateTime.Now.TimeOfDay;
+            string timeString = DateTime.Now.ToString("HH");    //get current time
+            int timeInt;
+            int.TryParse(timeString, out timeInt);              //parse string datetime to int
 
-            TimeSpan maru_maru = TimeSpan.Parse("20:30");       // 0000 midnight
-            TimeSpan maru_hito = TimeSpan.Parse("01:00");       // 0100 late night
-            TimeSpan maru_futa = TimeSpan.Parse("02:00");       // 0200 late night
-            TimeSpan maru_san = TimeSpan.Parse("03:00");        // 0300 late night
-            TimeSpan maru_yon = TimeSpan.Parse("04:00");        // 0400 late night
-            TimeSpan maru_go = TimeSpan.Parse("05:00");         // 0500 early morning
-            TimeSpan maru_roku = TimeSpan.Parse("06:00");       // 0600 early morning
-            TimeSpan maru_nana = TimeSpan.Parse("07:00");       // 0700 early morning
-            TimeSpan maru_hachi = TimeSpan.Parse("08:00");      // 0800 morning
-            TimeSpan maru_kyuu = TimeSpan.Parse("09:00");       // 0900 morning
-            TimeSpan hito_maru = TimeSpan.Parse("10:00");       // 1000 morning
-            TimeSpan hito_hito = TimeSpan.Parse("11:00");       // 1100 morning
-            TimeSpan hito_futa = TimeSpan.Parse("12:00");       // 1200 noon
-            TimeSpan hito_san = TimeSpan.Parse("13:00");        // 1300 afternoon                                 
-            TimeSpan hito_yon = TimeSpan.Parse("14:00");        // 1400 afternoon
-            TimeSpan hito_go = TimeSpan.Parse("15:00");         // 1500 afternoon
-            TimeSpan hito_roku = TimeSpan.Parse("16:00");       // 1600 evening
-            TimeSpan hito_nana = TimeSpan.Parse("17:00");       // 1700 evening
-            TimeSpan hito_hachi = TimeSpan.Parse("18:00");      // 1800 late evening
-            TimeSpan hito_kyuu = TimeSpan.Parse("19:00");       // 1900 night
-            TimeSpan futa_maru = TimeSpan.Parse("20:00");       // 2000 night
-            TimeSpan futa_hito = TimeSpan.Parse("20:31");       // 2100 night
-            TimeSpan futa_futa = TimeSpan.Parse("22:00");       // 2200 night
-            TimeSpan futa_san = TimeSpan.Parse("23:00");        // 2300 night */
+            string exePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);   //get exe file pathing
 
-            if(DateTime.Now.Hour == 17 && DateTime.Now.Minute == 31)
+            //switch time cases
+            switch (timeInt)
             {
-                string exePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-                string filePath = @"Resources\temp\Akizuki-01.wav";
-                string fullPath = Path.Combine(exePath, filePath);
+                case 00:
+                    string path0 = @"Resources\temp\Akizuki-00.wav";     //path of voiceline for time 0000
+                    String path00 = Path.Combine(exePath, path0);      //combine file pathing
 
-                audioPlayer.SoundLocation = fullPath;
-                audioPlayer.Play();
-            }       
+                    audioPlayer.SoundLocation = path00;       //told audioPlayer the voiceline location
+                    audioPlayer.Play();                         //play the voiceline
+                    break;
+                case 01:
+                    string path1 = @"Resources\temp\Akizuki-01.wav";     //path of voiceline for time 0000
+                    string fullPath = Path.Combine(exePath, path1);      //combine file pathing
+
+                    audioPlayer.SoundLocation = fullPath;       //told audioPlayer the voiceline location
+                    audioPlayer.Play();                         //play the voiceline
+                    break;
+                case 02:
+                    string path2 = @"Resources\temp\Akizuki-02.wav";     //path of voiceline for time 0000
+                    string path02 = Path.Combine(exePath, path2);      //combine file pathing
+
+                    audioPlayer.SoundLocation = path02;       //told audioPlayer the voiceline location
+                    audioPlayer.Play();                         //play the voiceline
+                    break;
+            }
         }
     }
 }
